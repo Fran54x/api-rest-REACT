3c0d6e499f5a32f8d0588dfb0ea63e8a5ead7eae
Luis Manuel Rios <riostorresGamer@gmail.com>
2023-02-06 11:45:03 -0600

    AR-Primera version api rest con crud

---
+++ api-rest/api-rest/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace api_rest.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItemDTO> TodoItems { get; set; } = null!;
+    }
+}
