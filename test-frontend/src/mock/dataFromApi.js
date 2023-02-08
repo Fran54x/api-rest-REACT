1efed2b3fa00f28e1d8e45d18909572e6e21619f
Abraham Meza <abraham.meza@sciodesk.com>
2023-02-08 17:47:00 -0600

    FIX aplicación frontend
    se reparó toda la aplicación frontend para comunicación
    con api rest

---
+++ test-frontend/src/mock/dataFromApi.js
@@ -0,0 +1,37 @@
+export const dataFromApi = [
+  {
+    id: 0,
+    name: "Abraham",
+    isComplete: true,
+  },
+  {
+    id: 1,
+    name: "Manuel",
+    isComplete: true,
+  },
+  {
+    id: 2,
+    name: "Wicho",
+    isComplete: true,
+  },
+  {
+    id: 3,
+    name: "Pedro",
+    isComplete: false,
+  },
+  {
+    id: 4,
+    name: "Maria",
+    isComplete: false,
+  },
+  {
+    id: 5,
+    name: "Miguel",
+    isComplete: true,
+  },
+  {
+    id: 6,
+    name: "Toto",
+    isComplete: false,
+  },
+];
